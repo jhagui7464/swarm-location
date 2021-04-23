da187d1b7e6ce3c0c1cc69b8a2989f27be218915
Jose Aguilar <jhagui7464@gmail.com>
2021-04-23 11:37:44 -0500

    start

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ShowScreen extends StatefulWidget {
+  @override
+  _ShowScreenState createState() => _ShowScreenState();
+}
+
+class _ShowScreenState extends State<ShowScreen> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(),
+    );
+  }
+}
