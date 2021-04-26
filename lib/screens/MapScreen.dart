50d25aa2542b8411c2770db6d04031fb464a3d30
Jose Aguilar <jhagui7464@gmail.com>
2021-04-26 14:02:22 -0500

    added markers

--- lib/screens/MapScreen.dart
+++ lib/screens/MapScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,6 +10,50 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   @override
+  Completer<GoogleMapController> _controller = Completer();
+  double lat;
+  double lon;
+  List<Marker> _markers = <Marker>[];
+
+  void translatePing(String test) {
+    List<String> listofCoordinates = [];
+
+    //we filter the string to be split into a list
+    String holder = '';
+    for (int i = 0; i < test.length; ++i) {
+      if (test[i] != ',') {
+        holder += test[i];
+      } else {
+        listofCoordinates.add(holder);
+        holder = '';
+      }
+    }
+    listofCoordinates.add(holder);
+
+    String nCoordinate;
+    String wCoordinate;
+    for (int i = 0; i < listofCoordinates.length; ++i) {
+      if (listofCoordinates[i] == 'N') {
+        nCoordinate = listofCoordinates[i - 1];
+      } else if (listofCoordinates[i] == 'W') {
+        wCoordinate = listofCoordinates[i - 1];
+      }
+    }
+
+    String holder1 = nCoordinate.substring(0, 2);
+    String holder2 = nCoordinate.substring(2);
+    lat = double.parse(holder1) + (double.parse(holder2) / 60);
+
+    holder1 = wCoordinate.substring(0, 3);
+    holder2 = wCoordinate.substring(3);
+    lon = -1 * (double.parse(holder1) + (double.parse(holder2) / 60));
+
+    _markers.add(Marker(
+        markerId: MarkerId('SomeId'),
+        position: LatLng(lat, lon),
+        infoWindow: InfoWindow(title: 'The title of the marker')));
+  }
+
   static final LatLng _kMapCenter =
       LatLng(19.018255973653343, 72.84793849278007);
 
@@ -23,7 +68,24 @@ class _MapScreenState extends State<MapScreen> {
       ),
       body: GoogleMap(
         initialCameraPosition: _kInitialPosition,
+        markers: Set<Marker>.of(_markers),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        myLocationEnabled: true,
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _refresh,
+        label: Text('Refresh'),
+        icon: Icon(Icons.directions_boat),
       ),
     );
+  }
+
+  Future<void> _refresh() async {
+    final GoogleMapController controller = await _controller.future;
+    translatePing("2611.5737,N,09813.4431,W");
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(lat, lon), zoom: 11.0, tilt: 0, bearing: 0)));
   }
 }
